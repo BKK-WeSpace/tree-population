8c07040737dd859fe57581008effff69a839863e
khongchai <world1955@hotmail.com>
2023-03-19 01:33:32 +0700

    updated test data while waiting for vallaris api key

--- src/data/repositories/TreesRepositories.ts
+++ src/data/repositories/TreesRepositories.ts
@@ -7,8 +7,11 @@ export default class TreeRepository {
   static getTreesWithinBound(
     reqBody: TreesRequestBody
   ): Promise<TreesResponse> {
-    // TODO replace with real data fetch to CouchBase.
+    // TODO replace with real data fetch to Valaris.
+
     // If the coordinates is empty, return ALL existing coordiantes from CouchBase.
     return Promise.resolve(mockResponse as TreesResponse);
   }
+
+  // TODO upload endpoint to update the detail of the tree. Needs to wait for valaris connection to be made first. Also implement the upload and response models.
 }
