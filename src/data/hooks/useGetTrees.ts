9999fae87bf7cde62be65378ae09cd546d4b71b7
khongchai <world1955@hotmail.com>
2023-03-19 02:13:47 +0700

    updated data fetching hook and tree return type

--- src/data/hooks/useGetTrees.ts
+++ src/data/hooks/useGetTrees.ts
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { useMemo } from "react";
+import Trees from "../../types/Trees";
 import TreeRepository from "../repositories/TreesRepositories";
 
-export function useGetTrees(polygon: number[][], deps: any[]) {
-  return useMemo(() => {
-    return TreeRepository.getTreesWithinBound({
-      polygon: polygon,
-    });
+export default function useGetTrees(
+  polygon: number[][],
+  deps: any[] = []
+): { trees?: Trees[]; isLoading: boolean } {
+  const [isLoading, setIsLoading] = useState(false);
+  const [trees, setTrees] = useState<Trees[]>([]);
+
+  async function fetchTrees() {
+    setIsLoading(true);
+    const trees = await TreeRepository.getTreesWithinBound({ polygon });
+    setIsLoading(false);
+    setTrees(trees);
+  }
+
+  useEffect(() => {
+    fetchTrees();
   }, deps);
+
+  return { isLoading, trees };
 }
