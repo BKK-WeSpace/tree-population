00c5991250cf8e674930dcc1f8e7037bc01d938e
Pavit Pimchanagul <pavitpim@gmail.com>
2023-03-19 15:26:43 +0700

    feat:setup theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,66 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import AboutUs from '../src/components/AboutUs/AboutUs'
-import reactLogo from './assets/react.svg'
-import Loading from './components/Loading'
-import useGetTrees from "./data/hooks/useGetTrees";
+import { useState, useEffect } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import './App.css';
+import AboutUs from '../src/components/AboutUs/AboutUs';
+import reactLogo from './assets/react.svg';
+import Loading from './components/Loading';
+import useGetTrees from './data/hooks/useGetTrees';
+
+const lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#94B044',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+     primary: {
+      main: '#94B044',
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isDark,setIsDark] = useState(true)
 
-  const {isLoading, trees} = useGetTrees(); // How to get tree from data hooks
+  const { isLoading, trees } = useGetTrees(); // How to get tree from data hooks
   console.log(isLoading, trees); // Delete this line when the frontend work begins.
 
   return (
-    <div className="App">
-      <Loading/>
-      <div>
-        <a href="#" target="_blank">
-          <img src="/logo.svg" className="logo" alt="WeSpace logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WeSpace + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <AboutUs />
+    <div className='App'>
+      <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
+        <Loading />
+        <div>
+          <a href='#' target='_blank'>
+            <img src='/logo.svg' className='logo' alt='WeSpace logo' />
+          </a>
+          <a href='https://reactjs.org' target='_blank'>
+            <img src={reactLogo} className='logo react' alt='React logo' />
+          </a>
+        </div>
+        <h1>WeSpace + React</h1>
+        <div className='card'>
+          <button onClick={() => setCount((count) => count + 1)}>
+            current Number of tree is : {count}
+          </button>
+
+          
+        </div>
+        <Typography variant='h5' color='primary'>
+            Bangkok OpenSource - Tree Mapping{' '}
+          </Typography>
+        <AboutUs />
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
