edb0ae1abc8a111c89f6614f3c9c3f922bfe4922
Pavit Pimchanagul <pavitpim@gmail.com>
2023-03-19 16:18:25 +0700

    feat: implement theme with getDesignToken function

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState, useMemo } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+// import IconButton from '@mui/material/IconButton';
+// import Brightness4Icon from '@mui/icons-material/Brightness4';
+// import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { getDesignTokens } from './theme/theme';
+import Button from '@mui/material/Button';
+
 import './App.css';
 import AboutUs from '../src/components/AboutUs/AboutUs';
 import reactLogo from './assets/react.svg';
 import Loading from './components/Loading';
+
 import useGetTrees from './data/hooks/useGetTrees';
 
-const lightTheme = createTheme({
-  palette: {
-    primary: {
-      main: '#94B044',
-    },
-    secondary: {
-      main: '#edf2ff',
-    },
-  },
-});
+// const lightTheme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#94B044',
+//     },
+//     secondary: {
+//       main: '#edf2ff',
+//     },
+//   },
+// });
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-     primary: {
-      main: '#94B044',
-    },
-  },
-});
+// const darkTheme = createTheme({
+//   palette: {
+//     mode: 'dark',
+//      primary: {
+//       main: '#94B044',
+//     },
+//   },
+// });
 
 function App() {
   const [count, setCount] = useState(0);
-  const [isDark,setIsDark] = useState(true)
+  const [mode,setMode] = useState('light')
 
   const { isLoading, trees } = useGetTrees(); // How to get tree from data hooks
   console.log(isLoading, trees); // Delete this line when the frontend work begins.
 
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  const handleTheme = () => setMode(mode==='light'? 'dark' : 'light')
+
   return (
+    <ThemeProvider theme={theme}>
     <div className='App'>
-      <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
+        <head>
+
+        </head>
         <Loading />
         <div>
           <a href='#' target='_blank'>
@@ -51,15 +65,16 @@ function App() {
           <button onClick={() => setCount((count) => count + 1)}>
             current Number of tree is : {count}
           </button>
-
+          <Button variant="outlined" onClick={handleTheme}>Theme : {mode.toUpperCase()}</Button>
           
         </div>
         <Typography variant='h5' color='primary'>
             Bangkok OpenSource - Tree Mapping{' '}
           </Typography>
         <AboutUs />
-      </ThemeProvider>
+ 
     </div>
+    </ThemeProvider>
   );
 }
 
