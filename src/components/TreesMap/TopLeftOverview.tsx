c82591b9071dd26af9788e7d3a8a5933c4300c68
T-angmo <natruja769@gmail.com>
2023-03-25 18:51:23 +0700

    feat: Update Style the Top Overview UI Widgets

--- src/components/TreesMap/TopLeftOverview.tsx
+++ src/components/TreesMap/TopLeftOverview.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import useGetTrees from "../../data/hooks/useGetTrees";
 import * as React from "react";
 import InputLabel from "@mui/material/InputLabel";
@@ -6,6 +6,7 @@ import MenuItem from "@mui/material/MenuItem";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 
 // TODO style this; apply theme, responsive, etc.
 export default function TopLeftOverview() {
@@ -14,6 +15,7 @@ export default function TopLeftOverview() {
   const treesInTheArea = isLoading ? "loading..." : data?.result?.length;
   // get array of area from ?
   const serveyArea = ["สวนลุมพินี", "สวนเบญ", "สวนจตุจักร", "สวนสนุก", "สวนน้ำ"];
+  const currentArea = serveyArea[0]
 
   console.log(data);
 
@@ -46,32 +48,55 @@ export default function TopLeftOverview() {
           color: "black",
         }}
       >
-        
-        <div>
-          พื้นที่สำรวจ
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Container>
+         <Typography
+         sx = {{
+          fontSize: '16px'
+         }}
+         >พื้นที่สำรวจ</Typography>
+          <FormControl sx={{
+            color: 'black' ,
+            minWidth: 150 ,
+            maxWidth: 150,
+            textAlign:"left",
+            mx:2
+            }}>
             <Select
+            IconComponent={KeyboardArrowDownOutlinedIcon}
               // value={serveyArea[0]}
               onChange={handleChange}
               displayEmpty
               inputProps={{ "aria-label": "Without label" }}
               defaultValue="option1"
               sx={{
-                color: 'black'
+                "& .MuiSvgIcon-root": { color: "#333333" },
+                color: '#333333',
+                backgroundColor: 'white',
+                boxShadow: "none",
+                ".MuiOutlinedInput-notchedOutline": { border: 0 },
+                "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline":
+                  {
+                    border: 0,
+                  },
+                "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                  {
+                    border: 0,
+                  },
               }}
             >
-              <MenuItem value="">
-                <em>All</em>
-              </MenuItem>
-              <MenuItem value={serveyArea[0]}>{serveyArea[0]}</MenuItem>
-              <MenuItem value={serveyArea[1]}>{serveyArea[1]}</MenuItem>
-              <MenuItem value={serveyArea[2]}>{serveyArea[2]}</MenuItem>
-              <MenuItem value={serveyArea[3]}>{serveyArea[3]}</MenuItem>
+
+            <MenuItem value="">
+              <em>All</em>
+            </MenuItem>
+            <MenuItem value={serveyArea[0]}>{serveyArea[0]}</MenuItem>
+            <MenuItem value={serveyArea[1]}>{serveyArea[1]}</MenuItem>
+            <MenuItem value={serveyArea[2]}>{serveyArea[2]}</MenuItem>
+            <MenuItem value={serveyArea[3]}>{serveyArea[3]}</MenuItem>
             </Select>
-            <FormHelperText>Without label</FormHelperText>
           </FormControl>
-        </div>
+          </Container>
       </Box>
+      
       <Box
         sx={{
           color: "black",
@@ -82,7 +107,10 @@ export default function TopLeftOverview() {
           boxShadow: "0px 4px 8px rgba(109, 143, 12, 0.11)",
         }}
       >
-        จำนวนต้นไม้ในพื้นที่ {treesInTheArea}
+        <Typography>จำนวนต้นไม้ในพื้นที่</Typography>
+        <Typography>{treesInTheArea} ต้น</Typography>
+        <Typography>สำหรับ {currentArea}</Typography>
+        
       </Box>
     </Box>
   );
