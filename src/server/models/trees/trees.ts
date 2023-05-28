7f81ff95b2c6688437deff4814193cc28b10daf9
khongchai <world1955@hotmail.com>
2023-05-28 14:03:33 +0700

    changed TODO

--- src/server/models/trees/trees.ts
+++ src/server/models/trees/trees.ts
@@ -14,6 +14,8 @@ export default interface Tree extends Omit<Feature<Point>, "properties"> {
     /**
      * Because we try to be opened to as many databases as possible, this properties object
      * will be a combination of all vendor-specific tree schemas.
+     *
+     * TODO create a json serializer for each of the tree schema. Can be strategies or factories, as long as they work and not too verbose...
      */
     properties?: WeSpaceTreeSchema & OpenStreetMapTreeSchema;
 }
