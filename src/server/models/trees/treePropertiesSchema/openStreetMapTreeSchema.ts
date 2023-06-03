db075d9fb052ebae93b4722019b5d110096736bf
Khongchai Greesuradej <56601823+Khongchai@users.noreply.github.com>
2023-06-03 20:05:32 +0700

    Updated tree schema (#170)
    * added tree property schema for OSM
    
    * changed TODO

---
+++ src/server/models/trees/treePropertiesSchema/openStreetMapTreeSchema.ts
@@ -0,0 +1,45 @@
+/**
+ * All possible tags of a tree on open street map as specified in
+ * https://wiki.openstreetmap.org/wiki/Tag:natural%3Dtree
+ *
+ * The tags are represented in a geojson as properties of a point, while in OSM, the
+ * tags are a properties in an XML-like document. To convert between the two formats, we'll
+ * have to use some of the tools discussed here https://wiki.openstreetmap.org/wiki/GeoJSON
+ */
+export class OpenStreetMapTreeSchema {
+    public natural: "tree";
+    public leafType?: "broadleaved" | "needleleaved";
+    public genus?: string;
+    public species?: string;
+    public speciesWikidata?: string;
+    public taxon?: string;
+    public sex?: "male" | "female";
+    public protected?: "yes" | "no";
+    public circumference?: number; // assuming in metres
+    public diameter?: number; // assuming in millimetres
+    public estHeight?: number; // assuming in metres
+    public height?: number; // assuming in metres
+    public diameterCrown?: number; // assuming in metres
+    public leafCycle?:
+        | "deciduous"
+        | "evergreen"
+        | "semi_deciduous"
+        | "semi_evergreen";
+
+    constructor(data: Omit<OpenStreetMapTreeSchema, "natural">) {
+        this.natural = "tree";
+        this.leafType = data?.leafType;
+        this.genus = data?.genus;
+        this.species = data?.species;
+        this.speciesWikidata = data?.speciesWikidata;
+        this.taxon = data?.taxon;
+        this.sex = data?.sex;
+        this.protected = data?.protected;
+        this.circumference = data?.circumference;
+        this.diameter = data?.diameter;
+        this.estHeight = data?.estHeight;
+        this.height = data?.height;
+        this.diameterCrown = data?.diameterCrown;
+        this.leafCycle = data?.leafCycle;
+    }
+}
--- src/server/models/trees/trees.ts
+++ src/server/models/trees/trees.ts
@@ -1,4 +1,6 @@
 import { Feature, Point } from "geojson";
+import { OpenStreetMapTreeSchema } from "./treePropertiesSchema/openStreetMapTreeSchema";
+import { WeSpaceTreeSchema } from "./treePropertiesSchema/wespaceTreeSchema";
 
 /**
  * The only mandatory fields for a tree are its longitude and latitude.
@@ -9,74 +11,11 @@ export default interface Tree extends Omit<Feature<Point>, "properties"> {
      * The id of this tree in the Vallaris database.
      */
     id: string;
-    properties?: Partial<TreeProperties>;
-}
-
-export interface TreeProperties {
     /**
-     * Whether or not the tree has been verified by a human.
-     */
-    isVerified: boolean;
-    imgId: string;
-    isReward: boolean;
-    commonName: string;
-    species: string;
-    genus: string;
-    quote: TreeQuote;
-    code: string;
-    isAlive: boolean;
-    health: "healthy" | "unhealthy";
-    crownSizeMeter: number;
-    crownShape: string;
-    lifeExpectancyYear: number;
-    descriptiveFeatures: string;
-    /**
-     * Animals that live off of a tree
-     */
-    arboreals: string[];
-    extinctArboreals: string[];
-    hasEcobenefits: boolean;
-    /**
-     * In the context of forestry, "DBH" stands for "Diameter at Breast Height".
-     * DBH is a standard measurement used to determine the size of a standing tree.
-     * It is typically measured at 1.3 meters (4.5 feet) above the ground, which is approximately the height of an adult's breast height.
+     * Because we try to be opened to as many databases as possible, this properties object
+     * will be a combination of all vendor-specific tree schemas.
      *
-     * Measuring DBH is an important forestry practice because it provides a consistent and accurate way to measure the size of trees,
-     * which is useful for forest inventory, growth analysis, and timber management. By measuring the DBH of a tree, foresters can estimate
-     * the tree's age, calculate its volume and biomass, and determine its value for timber harvesting.
+     * TODO create a json serializer for each of the tree schema. Can be strategies or factories, as long as they work and not too verbose...
      */
-    diameterAtBreastHeightInches: number;
-    archived: boolean;
-    crownLightexposure: "Partial" | "Full" | "None" | number;
-    location: Partial<TreeLocationData>;
-    management: Partial<TreeManagementDetail>;
-}
-
-export interface TreeAppearance {
-    circumference?: string;
-    height?: number;
-}
-
-export interface TreeQuote {
-    quote?: string;
-    description?: string;
-}
-
-export interface TreeLocationData {
-    /**
-     * A short description of where the tree is located.
-     */
-    shortDescription: string;
-    address: string;
-    no: string;
-    street: string;
-    district: string;
-    // How the land around the tree is being used. For example, it could be used for agriculture, residential or commercial purposes.
-    typeOfLandUse: string;
-    areaCharacteristics: string;
-}
-
-export interface TreeManagementDetail {
-    urgency: "low" | "mid" | "high";
-    decorations: string;
+    properties?: WeSpaceTreeSchema & OpenStreetMapTreeSchema;
 }
